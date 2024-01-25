194a77ab89bfb189d6d457b69931ad0dd8d33da9
sponbob-pat <ixdos1111@gmail.com>
2024-01-25 17:23:41 +0900

    문제 1 정답코드

--- src/DataStructure/Sum_001.java
+++ src/DataStructure/Sum_001.java
@@ -2,6 +2,25 @@ package DataStructure;
 
 public class Sum_001 {
 
-    int sum = 0;
+    public void main(int n, String target) {
+
+        /* 
+        Scanner sc = new Scanner(System.in); 
+        int n = sc.nextInt(); 
+        String target = sc.next(); 
+
+        */
+
+        int sum = 0; 
+        for (int i = 0; i < n; i++) {
+
+            sum += (target.charAt(i) - '0'); 
+
+        }
+
+        System.out.println(sum);
+
+    }
+
 
 }
