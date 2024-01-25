913701be0aa476bd7eb0f3764508cb6844a67196
sponbob-pat <ixdos1111@gmail.com>
2024-01-25 17:02:06 +0900

    basic

---
+++ src/DataStructure/Sum_001.java
@@ -0,0 +1,7 @@
+package DataStructure;
+
+public class Sum_001 {
+
+    int sum = 0;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import DataStructure.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        DataStructure.Sum_001 temp = new DataStructure.Sum_001();
+    }
+}
